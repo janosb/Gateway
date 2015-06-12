67ea463c2210f8b42141ec3fe26d2f231894a093
Janos Botyanszki - NERSC <jbotyanszki@gmail.com>
2015-06-11 20:19:11 -0700

    cleaned up comments

--- odetta/settings_local_template.py
+++ odetta/settings_local_template.py
@@ -4,7 +4,6 @@ DATABASES = {
         'NAME': 'odetta',                      # Or path to database file if using sqlite3.
         'USER': 'odetta_user',                      # Not used with sqlite3.
         'PASSWORD': 'xxxx',                  # Not used with sqlite3.
-        # 'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
         'HOST': 'scidb1.nersc.gov',
         'PORT': '5432',                      # Set to empty string for default. Not used with sqlite3.
     }
