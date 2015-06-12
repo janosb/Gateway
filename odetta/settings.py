607a78ad83d95ad17b61ba3b15aa4931612025c7
Janos Botyanszki - NERSC <jbotyanszki@gmail.com>
2015-06-11 20:15:10 -0700

    moving database settings to local file (settings_local.py)

--- odetta/settings.py
+++ odetta/settings.py
@@ -11,19 +11,7 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'odetta',                      # Or path to database file if using sqlite3.
-        'USER': 'odetta_user',                      # Not used with sqlite3.
-        'PASSWORD': 'xxxx',                  # Not used with sqlite3.
-        # 'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
-        'HOST': 'scidb1.nersc.gov',
-        'PORT': '5432',                      # Set to empty string for default. Not used with sqlite3.
-    }
-}
-
-FITS_ROOT = "/Users/tonywang/Documents/data/"
+FITS_ROOT = ""
 
 TEMPPASSWORD = "0d3tta"
 
@@ -162,3 +150,10 @@ LOGGING = {
         },
     }
 }
+
+
+try:
+    from settings_local import *
+except ImportError:
+    pass
+
