d6025e608bd66db1dd4dafce1e8402af14eaa47d
Janos Botyanszki - NERSC <jbotyanszki@gmail.com>
2015-06-11 20:01:51 -0700

    updated restart_server

--- restart_server.sh
+++ restart_server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-kill $(cat $PWD/django.pid)
-python manage.py runfcgi method=prefork host=127.0.0.1 port=3033 pidfile=$PWD/django.pid outlog=$PWD/django.out errlog=$PWD/django.err
+kill $(cat $PWD/django1.pid)
+python manage.py runfcgi method=prefork host=127.0.0.1 port=3033 pidfile=$PWD/django1.pid outlog=$PWD/django1.out errlog=$PWD/django1.err
